e475bfb0270ddfb5c00035e12581ecc8a6b17b19
KamilMarszalek <kamilmar553@gmail.com>
2025-11-14 12:53:26 +0100

    refactor UDP client and server: modularize argument parsing and socket creation; improve error handling in datagram processing

--- src/udp/python/udp_server.py
+++ src/udp/python/udp_server.py
@@ -6,30 +6,35 @@ HOST = "0.0.0.0"
 BUFSIZE = 1048576
 
 
-def main(args):
+def parse_port(args):
     if len(args) < 2:
-        print("no port passed efemeric port will be given")
-        port = 0
-    else:
-        port = int(args[1])
-
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((HOST, port))
-        print("Will listen on", s.getsockname()[0], ":", s.getsockname()[1])
-        echo(s)
+        print("no port passed, ephemeral port will be used")
+        return 0
+    return int(args[1])
 
 
-def echo(s: socket.socket):
+def create_udp_socket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, port))
+    real_port = s.getsockname()[1]
+    print(f"Server listening on {HOST}:{real_port}")
+    return s
+
+
+def echo_loop(sock: socket.socket):
     while True:
-        data_address = s.recvfrom(BUFSIZE)
-        data = data_address[0]
-        address = data_address[1]
-        datagram = Datagram.from_bytes(data)
-        # print("Message from client", str(address))
-        # print("Received datagram", str(datagram))
+        data, addr = sock.recvfrom(BUFSIZE)
         if not data:
-            break
-        s.sendto(datagram.to_bytes(), address)
+            continue
+
+        d = Datagram.from_bytes(data)
+        sock.sendto(d.to_bytes(), addr)
+
+
+def main(args):
+    port = parse_port(args)
+    with create_udp_socket(port) as s:
+        echo_loop(s)
 
 
 if __name__ == "__main__":
